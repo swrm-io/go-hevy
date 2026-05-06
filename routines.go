5bcf51275269a07c2800c16961cc2d042301598c
Richard Cox <code@swrm.io>
2026-05-05 22:26:51 -0700

    [feat] Pagninated Routines

--- routines.go
+++ routines.go
@@ -1,6 +1,10 @@
 package hevy
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
 
 // A response for fetching a list of workouts
 type routineResponse struct {
@@ -8,32 +12,89 @@ type routineResponse struct {
 	Routines []Routine `json:"routines"`
 }
 
-// Routines gets all routines.
-func (c Client) Routines() ([]Routine, error) {
+func (c Client) Routines() func(func(Routine) bool) {
+	size := 10
+	return func(yield func(Routine) bool) {
+		page := 1
+
+		for {
+			resp, next, err := c.GetRoutines(page, size)
+			if err != nil {
+				return
+			}
+
+			for _, routine := range resp {
+				if !yield(routine) {
+					return
+				}
+			}
+
+			if next == 0 {
+				break
+			}
+			page++
+		}
+	}
+}
+
+func (c Client) AllRoutines() ([]Routine, error) {
 	routines := []Routine{}
 
 	page := 1
-	pageCount := 10
-	for {
+	size := 10
 
-		q := map[string]string{
-			"page":      fmt.Sprintf("%d", page),
-			"pageCount": fmt.Sprintf("%d", pageCount),
-		}
-		url := c.constructURL("routines", q)
-		result := routineResponse{}
-		err := c.get(url, &result)
+	for {
+		resp, next, err := c.GetRoutines(page, size)
 		if err != nil {
 			return nil, err
 		}
 
-		routines = append(routines, result.Routines...)
+		routines = append(routines, resp...)
 
-		if result.Page == result.PageCount {
+		if next == 0 {
 			break
 		}
-		page++
+		page = next
 	}
 
 	return routines, nil
+}
+
+// Routines gets all routines.
+func (c Client) GetRoutines(page int, size int) ([]Routine, int, error) {
+	if size > 10 {
+		size = 10
+	}
+
+	q := map[string]string{
+		"page":     fmt.Sprintf("%d", page),
+		"pageSize": fmt.Sprintf("%d", size),
+	}
+	url := c.constructURL("routines", q)
+	result := routineResponse{}
+	err := c.get(url, &result)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	next := result.Page + 1
+	if result.Page == result.PageCount {
+		next = 0
+	}
+
+	return result.Routines, next, nil
+}
+
+func (c Client) Routine(id uuid.UUID) (Routine, error) {
+	path := fmt.Sprintf("routines/%s", id.String())
+	url := c.constructURL(path, map[string]string{})
+
+	result := Routine{}
+
+	err := c.get(url, &result)
+	if err != nil {
+		return Routine{}, err
+	}
+
+	return result, nil
 }
--- routines_test.go
+++ routines_test.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/swrm-io/go-hevy"
 )
@@ -22,6 +23,11 @@ func TestRoutine(t *testing.T) {
 			assert.NoError(t, err)
 			_, err = res.Write(data)
 			assert.NoError(t, err)
+		case "/v1/routines/9c3e6a25-67e9-4c0c-860c-286e13fe9924":
+			data, err := os.ReadFile("testdata/responses/single-routine.json")
+			assert.NoError(t, err)
+			_, err = res.Write(data)
+			assert.NoError(t, err)
 		}
 	}))
 	defer srv.Close()
@@ -29,10 +35,28 @@ func TestRoutine(t *testing.T) {
 	client := hevy.NewClient("my-fake-api-key")
 	client.APIURL = srv.URL
 
-	t.Run("Test Paginated Routines", func(t *testing.T) {
-		routines, err := client.Routines()
+	t.Run("Test All Routines", func(t *testing.T) {
+		routines, err := client.AllRoutines()
 		assert.NoError(t, err)
 		assert.NotEmpty(t, routines)
 		assert.Len(t, routines, 3)
+	})
+
+	t.Run("Test Paginated Routines", func(t *testing.T) {
+		routines, next, err := client.GetRoutines(2, 1)
+		assert.Equal(t, 3, next)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, routines)
+		assert.Len(t, routines, 1)
+	})
+
+	t.Run("Test Single Routine", func(t *testing.T) {
+		routineID, err := uuid.Parse("9c3e6a25-67e9-4c0c-860c-286e13fe9924")
+		assert.NoError(t, err)
+
+		routine, err := client.Routine(routineID)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, routine)
+		assert.Equal(t, "9c3e6a25-67e9-4c0c-860c-286e13fe9924", routine.ID.String())
 	})
 }
